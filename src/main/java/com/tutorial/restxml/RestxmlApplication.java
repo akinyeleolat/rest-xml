4533ddb21fcdb50c09ffd970a9a59e0c6ba923bf
Tosin Akinyele <akinyeleolat2005@gmail.com>
2021-07-15 22:09:56 +0100

    add implementation

---
+++ src/main/java/com/tutorial/restxml/RestxmlApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.restxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestxmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestxmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/restxml/RestxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.restxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
